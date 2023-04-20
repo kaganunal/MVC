25babc2f0e55a66e1067aecd4f40bd1bd6143d29
kaganunal <oguzkaganu@gmail.com>
2023-04-20 23:59:30 +0300

    Exercise

---
+++ 18April/18April/Controllers/TekCift-AsalController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace _18April.Controllers
+{
+    public class TekCift_AsalController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i < 11; i++)
+            {
+                numbers.Add(i);
+            }
+            return View(numbers);
+        }
+    }
+}
